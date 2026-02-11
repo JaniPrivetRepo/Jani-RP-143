76e0f77d919ccd7969cb310522f87c4842c6a685
JaniPrivetRepo <fffdfhhbnjjh@gmail.com>
2026-02-12 00:53:15 +0530

    Update thumbnails.py

--- IstkharMusic/utils/thumbnails.py
+++ IstkharMusic/utils/thumbnails.py
@@ -137,7 +137,7 @@ async def get_thumb(videoid):
             stroke_fill="black",
         )
 
-        stats_text = f"YouTube : {views} | Time : {duration} | Player : @MusarratMusicBot"
+        stats_text = f"YouTube : {views} | Time : {duration} | Player : @Jani_Music_Robot"
         w_stats = get_text_width(stats_text, font_details)
         draw.text(
             ((1280 - w_stats) / 2, text_y_pos + 70),
